90871a0902a3a17a73bd44c2ce3297cf7337569b
E.P.S <58166775+estrellapsanz@users.noreply.github.com>
2022-05-24 19:51:08 +0200

    wip

--- src/app/components/cat/cat.component.ts
+++ src/app/components/cat/cat.component.ts
@@ -11,6 +11,8 @@ export class CatComponent implements OnInit {
   cat!: any;
   img!: string;
   showDetails: boolean;
+  default_cat: any;
+
   constructor(
     private catService: CatsService,
     private activatedRoute: ActivatedRoute,
@@ -20,16 +22,26 @@ export class CatComponent implements OnInit {
   ngOnInit(): void {
     const identifier: any = this.activatedRoute.snapshot.paramMap.get('id');
 
-    //console.log('Identifier -->' + identifier);
+    //when cat not found
+    if (identifier == '123') {
+      this.cat.breeds[0].name = 'Isidoro';
+      this.cat.breeds[0].origin = 'Cartoons';
+      this.cat.url = 'assets/default.jpg';
+      this.cat.breeds[0].description = 'The best cartoon of a cat ever seen!';
+      this.cat.breeds[0].temperament = 'Lovely and sweet althougt very smart.';
+      this.cat.breeds[0].affection_level = '10';
+      this.cat.breeds[0].wikipedia_url =
+        'https://es.wikipedia.org/wiki/Heathcliff';
+      this.cat.breeds[0].vetstreet_url = 'cat.breeds[0].vetstreet_url';
+      this.cat.breeds[0].child_friendly = 10;
+    }
+
     this.catService.getCatImageById(identifier).subscribe((cat) => {
       if (!cat) {
         return this.router.navigateByUrl('/');
       }
 
       this.cat = cat;
-      //console.log(this.cat);
-      //console.log(this.cat.breeds[0].description);
-      //console.log(this.cat.url);
       return this.router.navigateByUrl('/cat/' + identifier);
     });
   }
