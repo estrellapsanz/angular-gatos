cf1c97525a7b2ca847ef2e7b901e114531b5ba4b
E.P.S <58166775+estrellapsanz@users.noreply.github.com>
2022-05-25 17:15:35 +0200

    wip

--- src/app/components/cat/cat.component.ts
+++ src/app/components/cat/cat.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CatsService } from 'src/app/services/cats.service';
@@ -12,50 +13,43 @@ export class CatComponent implements OnInit {
   img!: string;
   showDetails: boolean;
   default_cat: any;
+  loading: boolean;
+  loaded: boolean;
 
   constructor(
     private catService: CatsService,
     private activatedRoute: ActivatedRoute,
     private router: Router
-  ) {}
+  ) {
+    this.loading = false;
+    this.loaded = false;
+  }
 
   ngOnInit(): void {
     const identifier: any = this.activatedRoute.snapshot.paramMap.get('id');
+    let errorResponse: any;
+    this.loading = true;
+    this.loaded = false;
 
-    //when cat not found
-    if (identifier == '123') {
-      this.cat.breeds[0].name = 'Isidoro';
-      this.cat.breeds[0].origin = 'Cartoons';
-      this.cat.url = 'assets/default.jpg';
-      this.cat.breeds[0].description = 'The best cartoon of a cat ever seen!';
-      this.cat.breeds[0].temperament = 'Lovely and sweet althougt very smart.';
-      this.cat.breeds[0].affection_level = '10';
-      this.cat.breeds[0].wikipedia_url =
-        'https://es.wikipedia.org/wiki/Heathcliff';
-      this.cat.breeds[0].vetstreet_url = 'cat.breeds[0].vetstreet_url';
-      this.cat.breeds[0].child_friendly = 10;
-    }
-
-    this.catService.getCatImageById(identifier).subscribe((cat) => {
-      if (!cat) {
-      
-        this.cat.breeds[0].name = 'Isidoro';
-        this.cat.breeds[0].origin = 'Cartoons';
-        this.cat.url = 'assets/default.jpg';
-        this.cat.breeds[0].description = 'The best cartoon of a cat ever seen!';
-        this.cat.breeds[0].temperament =
-          'Lovely and sweet althougt very smart.';
-        this.cat.breeds[0].affection_level = '10';
-        this.cat.breeds[0].wikipedia_url =
-          'https://es.wikipedia.org/wiki/Heathcliff';
-        this.cat.breeds[0].vetstreet_url = 'cat.breeds[0].vetstreet_url';
-        this.cat.breeds[0].child_friendly = 10;
-        return this.router.navigateByUrl('/');
+    this.catService.getCatImageById(identifier).subscribe(
+      (cat) => {
+        if (!cat) {
+          return this.router.navigateByUrl('/');
+        } else {
+          this.loading = false;
+          this.loaded = true;
+          this.cat = cat;
+        }
+        return this.router.navigateByUrl('/cat/' + identifier);
+      },
+      (error: HttpErrorResponse) => {
+        errorResponse = error.error;
+        this.loaded = false;
+        this.loading = false;
+        console.log(errorResponse);
+        return this.router.navigateByUrl('/cat/' + identifier);
       }
-
-      this.cat = cat;
-      return this.router.navigateByUrl('/cat/' + identifier);
-    });
+    );
   }
 
   toggleShow(): void {
